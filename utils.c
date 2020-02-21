0d4994530efce5ff9a53942aca3b46ea80e6008b
lappy <adamrnasi@gmail.com>
2020-02-21 14:34:43 -0500

    refactored vertical and horizontal check

--- utils.c
+++ utils.c
@@ -9,7 +9,10 @@ player checkHorizontal(player board[6][7]);
 
 player checkVertical(player board[6][7]);
 
+player checkHorizontalAndVertical(player board[6][7]);
+
 player checkWins(player board[6][7]) {
+    return checkHorizontalAndVertical(board);
     player winner = checkHorizontal(board);
     if (winner != NONE)
         return winner;
@@ -49,6 +52,40 @@ player checkVertical(player board[6][7]) {
             }
             if (count > 3)
                 return countPlayer;
+        }
+    }
+    return NONE;
+}
+
+player checkHorizontalAndVertical(player board[6][7]) {
+    int countH = 0;
+    int countV = 0;
+    player countPlayerH;
+    player countPlayerV;
+    for (int i = 0; i < 7; i++) {
+        countPlayerH = NONE;
+        countPlayerV = NONE;
+        for (int j = 0; j < 7; j++) {
+            if (i < 6) {
+            if (board[i][j] != NONE && board[i][j] == countPlayerH) {
+                countH++;
+            } else {
+                countPlayerH = board[i][j];
+                countH = 1;
+            }
+            if (countH > 3)
+                return countPlayerH;
+            }
+            if (j < 6) {
+                if (board[j][i] != NONE && board[j][i] == countPlayerV) {
+                    countV++;
+                } else {
+                    countPlayerV = board[j][i];
+                    countV = 1;
+                }
+                if (countV > 3)
+                    return countPlayerV;
+            }
         }
     }
     return NONE;
